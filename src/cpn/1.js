2aa276e8e50f8b42092eef8956f621595dfe7432
hw <hyeonu4945@gmail.com>
2022-10-24 00:13:12 +0900

    8prop

--- src/cpn/1.js
+++ src/cpn/1.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello(){
-            
+export default function Hello({age}){
+    
     // var name ="mike"
     const [name, setName]=useState('mike')
-
+    const msg = age>19?"성인":"학생"
 
     return(
 
     <div>
 
-        <h2 id="name">{name}</h2>
+        <h2 id="name">{name}({age}:{msg})</h2>
+        
+        <UserName name = {name}/>
+
         <button onClick={()=>{
         setName(name==="mike"?"jane":"mike")
+        
         }}>change</button>
 
       
